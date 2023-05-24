adc296cf4b18353524317a15df3c67cd03067a2e
Juned Dhanga <jadhanga@enacton.com>
2023-05-24 18:45:19 +0530

    UXI, PM live section UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          DEFAULT: '1380px'
+        },
+      },
       fontFamily: {
         'anomalia': ['anomalia'],
       },
@@ -13,14 +20,20 @@ module.exports = {
           900: "#010C27",
         },
       },
-    },
-    container: {
-      center: true,
-      padding: "1rem",
-      screens: {
-        DEFAULT: '1380px'
+      fontSize: {
+        '20px': ['20px', '35px'],
+        '32px': ['32px', '46px'],
+        '50px': ['50px', '52px'],
       },
+      lineHeight:{
+        52:"52px"
+      },
+      backgroundImage:{
+        'dark-900-overlay': 'linear-gradient(180deg, #010C27 0%, #01091C 21.82%, #010C27 100%)',
+        'dark-overlay': 'linear-gradient(90deg, #010C27 33.59%, rgba(1, 12, 39, 0.74) 73.48%, rgba(1, 12, 39, 0) 100%);'
+      }
     },
+    
   },
   plugins: [],
 
