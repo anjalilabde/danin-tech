7ef906e92935f31fb161a692133b50b54f5b0083
Juned Dhanga <jadhanga@enacton.com>
2023-05-25 16:39:21 +0530

    Mob navlink onclick JS

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,30 @@ window.onload = () => {
     const mobMenuInner = document.querySelector("#mob-menu-inner");
     const menuToggleButton = document.querySelector("#hamburger-button");
     const menuCloseButton = document.querySelector("#menu-closer");
+    const mobNavLinks = document.querySelectorAll('#mob-menu-inner ul li');
+
+    const showMenu = () => {
+        mobMenuInner.classList.add('translate-x-0');
+        mobMenuInner.classList.remove("ltr:-translate-x-full", "rtl:translate-x-full");
+        mobMenuOverlay.classList.remove("hidden");
+    }
+    const hideMenu = () => {
+        mobMenuInner.classList.remove('translate-x-0');
+        mobMenuInner.classList.add("ltr:-translate-x-full", "rtl:translate-x-full");
+        mobMenuOverlay.classList.add("hidden");
+    }
 
     menuToggleButton.onclick = () => {
-      mobMenuInner.classList.add('translate-x-0');
-      mobMenuInner.classList.remove("ltr:-translate-x-full", "rtl:translate-x-full");
-      mobMenuOverlay.classList.remove("hidden");
+        showMenu();
     };
     menuCloseButton.onclick = () => {
-      mobMenuInner.classList.remove('translate-x-0');
-      mobMenuInner.classList.add("ltr:-translate-x-full", "rtl:translate-x-full");
-      mobMenuOverlay.classList.add("hidden");
-
+        hideMenu();
     };
+
+    for (const mobNavLink of mobNavLinks) {
+        mobNavLink.onclick = () =>{
+            hideMenu();
+        }
+      }
+  
   }
